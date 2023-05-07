69e844ee061f67ec99e9a16425ca0dc23ecf2eb9
Tim Kent <timdekent@gmail.com>
2023-05-06 21:06:07 -0700

    add dup recurring todo item when checked

--- src/db/functions/check-todo-sql.ts
+++ src/db/functions/check-todo-sql.ts
@@ -6,6 +6,7 @@ RETURNS bool LANGUAGE plpgsql AS
 $func$
 DECLARE
 ischecked bool;
+isrecur bool;
 BEGIN
 ischecked := (
   SELECT is_checked
@@ -13,7 +14,14 @@ ischecked := (
   WHERE todo_item_id = i_id
   AND list_id = l_id
   AND user_id = u_id
-); 
+);
+isrecur := (
+  SELECT is_recurring
+  FROM items_todo
+  WHERE todo_item_id = i_id
+  AND list_id = l_id
+  AND user_id = u_id
+);
 IF ischecked IS NULL THEN
 RETURN false;
 ELSE
@@ -26,6 +34,32 @@ ELSE
     UPDATE todo_subtasks
     SET is_checked = true
     WHERE todo_item_id = i_id;
+
+    IF (isrecur = true) THEN
+      INSERT INTO items_todo
+        (
+          list_id,
+          user_id,
+          item_name,
+          item_category,
+          item_location,
+          time_due,
+          is_recurring,
+          recurrence_value
+        )
+      SELECT
+        l_id,
+        u_id,
+        item_name,
+        item_category,
+        item_location,
+        time_due,
+        true,
+        recurrence_value
+      FROM items_todo
+      WHERE todo_item_id = i_id;
+    END IF;
+  
   ELSE
     UPDATE items_todo
     SET
