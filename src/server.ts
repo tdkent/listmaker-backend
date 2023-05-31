3767f6a91c424accc8e4537627957d9023b03a29
Tim Kent <timdekent@gmail.com>
2023-05-31 14:42:46 -0700

    update cors

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ app.use(json());
 // );
 
 app.use(cors(), function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://deft-torte-c93e9a.netlify.app");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
